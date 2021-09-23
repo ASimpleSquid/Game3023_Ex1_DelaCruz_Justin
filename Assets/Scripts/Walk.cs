e562a081a687f7a9db2b6fb3e1bdb61d145d99cc
Justin Dela Cruz <Traxiliongames@gmail.com>
2021-09-23 16:25:15 -0400

    Applied sprites, script for movement, and level for testing

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    // This is to ensure the speed is set at a stable 1. This can be modified in Unity
+    public float WalkSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The Horizontal Axis ensures we can move left and right, with transform position
+        // we can move around using time and our walk speed
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * WalkSpeed;
+    }
+}
