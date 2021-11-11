9484ed4bbd5085653e5c0305b7b505da1a1047d0
Justin Dela Cruz <Traxiliongames@gmail.com>
2021-11-11 14:17:18 -0500

    Changed main sprite, Sprite has run, idle, and jump animation can...
    Change directions too

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -5,20 +5,60 @@ using UnityEngine;
 public class Walk : MonoBehaviour
 {
     // This is to ensure the speed is set at a stable 1. This can be modified in Unity
-    public float WalkSpeed = 1;
+    public float WalkSpeed;
+    public float JumpForce;
+    float Speed = 5;
+    float horizontalMove = 0f;
+
+    public Animator animator;
+
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        WalkSpeed = 1f;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        // The Horizontal Axis ensures we can move left and right, with transform position
-        // we can move around using time and our walk speed
+    { 
+        horizontalMove = Input.GetAxisRaw("Horizontal") * WalkSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * WalkSpeed;
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime* Speed;
+
+
+        if (!Mathf.Approximately(0, movement))
+        {
+            transform.rotation = movement< 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        if (Input.GetButtonDown("Jump") && _rigidbody.velocity.y == 0)
+        {
+            _rigidbody.AddForce(Vector2.up * 50);
+        }
+
+
+
+        if (_rigidbody.velocity.y == 0)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+        }
+
+        if (_rigidbody.velocity.y > 0)
+        {
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (_rigidbody.velocity.y < 0)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
     }
 }
